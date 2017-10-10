6c4f8351c476ee85d39624d6e70d9cc41dbdd539
PC19 <pc19software@gmail.com>
2017-10-10 13:03:12 -0300

    First commit

---
+++ src/services/mysql/categories.js
@@ -0,0 +1,59 @@
+const categories = deps => {
+	return {
+		all: () => {
+			return new Promise((resolve, reject) => {
+				const { connection, errorHandler } = deps
+				connection.query('SELECT * FROM categories', (error, results) => {
+					if (error) {
+						errorHandler(error, 'Falha ao listar as categorias', reject)
+						return false
+					}
+
+					resolve({ categories: results })
+				})
+			})
+		},
+		save: (name) => {
+			return new Promise((resolve, reject) => {
+				const { connection, errorHandler } = deps
+				connection.query('INSERT INTO categories (name) VALUES (?)', [name], (error, results) => {
+					if (error) {
+						errorHandler(error, `Falha ao salvar a categorias ${name}`, reject)
+						return false
+					}
+
+					resolve({ category: { name, id: results.insertId } })
+				})
+			})
+		},
+		update: (id, name) => {
+			return new Promise((resolve, reject) => {
+				const { connection, errorHandler } = deps
+				connection.query('UPDATE categories SET name = ? WHERE id = ?', [name, id], (error, results) => {
+					if (error) {
+						errorHandler(error, `Falha ao atualizar a categorias ${name}`, reject)
+						return false
+					}
+
+					resolve({ category: { name, id }, affectedRows: results.affectedRows })
+				})
+			})
+		},
+		del: (id) => {
+			return new Promise((resolve, reject) => {
+				const { connection, errorHandler } = deps
+				connection.query('DELETE FROM categories WHERE id = ?', [id], (error, results) => {
+					if (error) {
+						errorHandler(error, `Falha ao excluir a categoria de id ${id}`, reject)
+						return false
+					}
+
+					resolve({ message: 'Categoria removida com sucesso', affectedRows: results.affectedRows })
+				})
+			})
+		}
+
+	}
+}
+
+module.exports = categories
